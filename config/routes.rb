c6c3631e7e74ae9cd7032ca7e943428384b45212
tassan <kndf_tassan0823@ezweb.ne.jp>
2017-08-02 06:43:16 +0000

    DB migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get 'events/new'
-
   get 'static_pages/home'
+  
+  get '/list', to: 'static_pages#list'
   root 'static_pages#home'
-
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
