2927e8b7a9b3c23500ffe02d2751954cdc0c132f
masa1511409 <muroi09079@gmail.com>
2017-08-06 07:59:33 +0000

    search_button

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
       @events = Event.all
+
       if params[:course].present?
         @events = @events.get_by_course params[:course]
         #@events = Event.find(params[:course])
