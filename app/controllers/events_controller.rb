05966f245c373341194d3a3100979b9a83e4b6aa
masa1511409 <muroi09079@gmail.com>
2017-08-06 23:40:23 +0000

    Participation_created

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,6 +14,7 @@ class EventsController < ApplicationController
   end
   
   def show
+
   end
   
   private
--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,15 +1,14 @@
 class RelationshipsController < ApplicationController
-    before_action :logged_in_user
 
   def create
-    user = User.find(params[:followed_id])
-    current_user.follow(user)
-    redirect_to user
+    event = User.find(params[:followed_id])
+    current_user.follow(event)
+    redirect_to root_path
   end
 
   def destroy
-    user = Relationship.find(params[:id]).followed
-    current_user.unfollow(user)
-    redirect_to user
+    event = Relationship.find(params[:id]).followed
+    current_user.unfollow(event)
+    redirect_to root_path
   end
 end
