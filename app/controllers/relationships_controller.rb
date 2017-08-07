74af6dd19138889a9392090558783902dc1e4348
masa1511409 <muroi09079@gmail.com>
2017-08-07 02:06:02 +0000

    参加、取り消し確認

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,14 +1,18 @@
 class RelationshipsController < ApplicationController
 
   def create
-    event = User.find(params[:followed_id])
+    #event = User.find(params[:followed_id])
+    event = Event.find(params[:followed_id])
     current_user.follow(event)
-    redirect_to root_path
+    flash[:success] = 'イベントへの参加を承りました.'
+    redirect_to event
+    #redirect_to show
   end
 
   def destroy
     event = Relationship.find(params[:id]).followed
     current_user.unfollow(event)
-    redirect_to root_path
+    flash[:success] = 'イベントへの参加を取り消しました.'
+   redirect_to event
   end
 end
